b57817218bcfdea8bb77ef9b891f033a54e73fe6
tosinori-m <your.email@example.com>
2022-06-23 10:51:23 +0900

    vue

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    これはコンポーネントから表示されています。
+    <p>{{ message }}</p>
+    <button @click="onClick">親メソッドを実行するボタン</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ['message'],
+  methods: {
+  onClick() {
+    this.$emit('toggle')
+    }
+  }
+}
+</script>
--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">トグルする</button>
-    <p>{{ text }}</p>
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています" />
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
 export default {
+    components: {
+    TestComponent
+  },
   data () {
     return {
       title: '初めてのVue.jsアプリです！',
@@ -32,3 +35,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
