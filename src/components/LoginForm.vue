ecb4a8b4eec466c208713f4b69956bd2fd0d5bb0
tosinori-m <your.email@example.com>
2022-06-24 10:00:48 +0900

    Add vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
     <h2>ログイン</h2>
-    <form>
+    <form @submit.prevent="login">
       <input type="email" required placeholder="メールアドレス" v-model="email">
       <input type="password" required placeholder="パスワード" v-model="password">
+      <div class="error">{{ error }}</div>
       <button>ログインする</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        this.error = null
+
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+          }
+        )
+        if (!res) {
+          throw new Error('メールアドレスかパスワードが違います')
+        }
+
+        console.log({ res })
+
+        return res
+      } catch (error) {
+        console.log({ error })
+        this.error = 'メールアドレスかパスワードが違います'
+      }
     }
   }
 }
