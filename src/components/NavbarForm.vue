8e1153e872a19d495c018790b50d0eae8c71e932
tosinori-m <your.email@example.com>
2022-06-25 10:54:37 +0900

    Add vue

--- src/components/NavbarForm.vue
+++ src/components/NavbarForm.vue
@@ -3,17 +3,53 @@
     <div>
       <p>こんにちは、<span class="name">{{ name }}</span>さん</p>
       <p class="email">現在、 {{ email }} でログイン中です</p>
+      <div class="error">{{ error }}</div>
     </div>
-    <button>ログアウト</button>
+    <button @click="logout">ログアウト</button>
   </nav>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       name: window.localStorage.getItem('name'),
-      email: window.localStorage.getItem('uid')
+      email: window.localStorage.getItem('uid'),
+      error: null
+    }
+  },
+  methods: {
+
+    async logout () {
+      this.error = null
+      try {
+        const res = await axios.delete('http://localhost:3000/auth/sign_out', {
+          headers: {
+            uid: this.email,
+            "access-token": window.localStorage.getItem('access-token'),
+            client: window.localStorage.getItem('client')
+          }
+        })
+
+        if (!res) {
+          new Error('ログアウトできませんでした')
+        }
+
+        if (!this.error) {
+          console.log("ログアウトしました")
+          window.localStorage.removeItem('access-token')
+          window.localStorage.removeItem('client')
+          window.localStorage.removeItem('uid')
+          window.localStorage.removeItem('name')
+
+          this.$router.push({ name: 'Welcome' })
+        }
+
+        return res
+      } catch (error) {
+        this.error = 'ログアウトできませんでした'
+      }
     }
   }
 }
