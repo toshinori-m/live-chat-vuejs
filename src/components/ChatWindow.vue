caa4138f940ec105f4d63057990a7fc5f9ebd158
tosinori-m <your.email@example.com>
2022-06-26 18:12:59 +0900

    Add vue

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="chat-window">
+    <div v-if="messages" class="messages">
+      <ul v-for="message in messages" :key="message.id">
+        <li :class="{ received: message.email !== uid, sent: message.email === uid }">
+          <span class="name">{{ message.name }}</span>
+          <span class="message">{{ message.content }}</span>
+          <span class="created-at">{{ message.created_at }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['messages'],
+  data () {
+    return {
+      uid: localStorage.getItem('uid')
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .chat-window {
+    background: white;
+    padding: 30px 20px;
+    border-bottom: 1px solid #eee;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  ul li {
+    display:inline-block;
+    clear: both;
+  }
+  .received .message {
+    background: #eee;
+    padding: 10px;
+    display: inline-block;
+    border-radius: 30px;
+    margin-bottom: 2px;
+    max-width: 400px;
+  }
+  .received {
+    float: left;
+  }
+  .sent {
+    float: right;
+  }
+  .sent .message {
+    background: #677bb4;
+    color: white;
+    padding: 10px;
+    display: inline-block;
+    border-radius: 30px;
+    margin-bottom: 2px;
+    max-width: 400px;
+  }
+  .name {
+    position: relative;
+    margin-right: 6px;
+    display: block;
+    font-size: 13px;
+  }
+  .created-at {
+    display: block;
+    color: #999;
+    font-size: 12px;
+    margin-bottom: 20px;
+    margin-left: 4px;
+  }
+  .messages {
+    max-height: 400px;
+    overflow: auto;
+  }
+</style>
--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="container">
     <NavbarForm />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
-import NavbarForm from '../components/NavbarForm.vue'
-
+import NavbarForm from '../components/NavbarForm'
+import ChatWindow from '../components/ChatWindow'
+import axios from 'axios'
 export default {
-  components: { NavbarForm },
+    components: { NavbarForm, ChatWindow },
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  mounted() {
+    this.getMessages()
+  },
 }
 </script>
 
